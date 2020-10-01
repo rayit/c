28439bd47852de0d40a071b428f9d3387ccaf0bf
rayit <rayit@rayitx1.home.rayit.com>
2020-10-01 23:01:10 +0200

    Enhanced error handling

--- sqlite_version2.c
+++ sqlite_version2.c
@@ -9,11 +9,19 @@ int main(void)
 	sqlite3_stmt *res;
 	int rc = sqlite3_open(":memory:", &db);
 	if ( rc != SQLITE_OK) {
+		puts("Status: 500 Internal Server Error\r");
+		puts("Content-Type: text/html\r");
+		puts("\r");
 		fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
 		sqlite3_close(db);
+		return 1;
 	}
 	rc =  sqlite3_prepare_v2(db, "SELECT SQLITE_VERSION()", -1, &res, 0);
 	if ( rc != SQLITE_OK) {
+		puts("Status: 500 Internal Server Error\r");
+		puts("Content-Type: text/html\r");
+		puts("\r");
+		puts("Error executing SELECT SQLITE_VERSION()");
 		sqlite3_close(db);
 		return 1;
 	}
