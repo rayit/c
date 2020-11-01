e46538f118103606038d885a7c2f91d7f17a8bca
rayit <rmarx@rayit.com>
2020-11-01 14:55:14 +0100

    Login form..

--- todo/js/login.js
+++ todo/js/login.js
@@ -10,7 +10,26 @@ export default class Login {
 
     handleSubmitForm(e) {
         e.preventDefault();
-        let input = document.querySelector('input');
-        console.log('Login form clicked');
+        let username = this.loginForm.querySelector('input[name="username"]');
+        let password = this.loginForm.querySelector('input[name="password"]');
+        this.login("POST", "/cgi-bin/login", username, password);
+    }
+
+
+    login(method = get, url, username, password) {
+        return new Promise(function (resolve, reject) {
+            let http = new XMLHttpRequest();
+            let params = "username="+username+"&password=" + password;
+            http.open(method, url, true);
+
+            // Send the proper header information along with the request
+            http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            http.onreadystatechange = function() {
+                if( http.readyState == 4 && http.status == 200) {
+                    alert(http.responseText);
+                }
+            }
+            http.send(params);
+        });
     }
 }
