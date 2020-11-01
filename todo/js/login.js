768b9399f5badc958f557a372dbf9667bb301dfc
rayit <rmarx@rayit.com>
2020-11-01 15:03:40 +0100

    Corrected post username and password value

--- todo/js/login.js
+++ todo/js/login.js
@@ -10,8 +10,8 @@ export default class Login {
 
     handleSubmitForm(e) {
         e.preventDefault();
-        let username = this.loginForm.querySelector('input[name="username"]');
-        let password = this.loginForm.querySelector('input[name="password"]');
+        let username = this.loginForm.querySelector('input[name="username"]').value``;
+        let password = this.loginForm.querySelector('input[name="password"]').value;
         this.login("POST", "/cgi-bin/login", username, password);
     }
 
