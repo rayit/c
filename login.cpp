5b56362af5dcbf9afb6d366cafff5852babc14dc
rayit <rayit@rayitx1.home.rayit.com>
2020-12-15 14:18:40 +0100

    Cleanup

--- login.cpp
+++ login.cpp
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include "Db.h"
 #include <stdio.h>
 #include <iostream>
 
@@ -7,11 +6,6 @@ using namespace std;
 
 int main()
 {
-
-//    Db db;
-//    int t;
-//    t = db.test();
-
     // Return
     puts("Status: 200 OK\r");
     puts("Content-Type: text/html\r");
